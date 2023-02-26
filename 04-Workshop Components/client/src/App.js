18db6297f9e30f9a618183076555281d04390251
ptteya <petq_1002@abv.bg>
2023-02-26 13:50:55 +0200

    Added Search component

--- 04-Workshop Components/client/src/App.js
+++ 04-Workshop Components/client/src/App.js
@@ -1,14 +1,22 @@
 import { Header } from "./components/common/Header";
 import { Footer } from "./components/common/Footer";
+import { Search } from "./components/Search/Search";
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
