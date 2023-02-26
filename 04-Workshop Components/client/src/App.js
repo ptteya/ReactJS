691685fa1bded0f2d6685e0c297e4710a70aa0ac
ptteya <petq_1002@abv.bg>
2023-02-26 13:41:12 +0200

    Added Footer

--- 04-Workshop Components/client/src/App.js
+++ 04-Workshop Components/client/src/App.js
@@ -1,10 +1,12 @@
 import { Header } from "./components/common/Header";
+import { Footer } from "./components/common/Footer";
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
