54f6ab12ed2ab771e13c86f2399689982f3d6800
ptteya <petq_1002@abv.bg>
2023-02-26 16:18:19 +0200

    Added UserItem and userService

--- 04-Workshop Components/client/src/App.js
+++ 04-Workshop Components/client/src/App.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService';
+
 import { Header } from "./components/common/Header";
 import { Footer } from "./components/common/Footer";
 import { Search } from "./components/Search/Search";
 import { UserSection } from "./components/UserSection.js/UserSection";
 import './App.css';
 
+
 function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        userService.getAll()
+            .then(users => setUsers(users));
+    }, []);
+
     return (
         <div>
             <Header />
@@ -12,7 +24,8 @@ function App() {
             <main className="main">
                 <section className="card users-container">
                     <Search />
-                    <UserSection />
+
+                    <UserSection users={users} />
                 </section>
             </main>
 
